847af700946773742cbad95b5b1d9ac649ce51f4
Benson <bensongathu23@gmail.com>
2021-06-18 22:09:40 +0300

    views added

--- app/request.py
+++ app/request.py
@@ -21,30 +21,7 @@ def get_quotes():
 
         if get_quotes_response:
             quote_results_list = get_quotes_response
-            quote_results = process_results(quote_results_list)
+            quote_results = quote_results_list            
 
-
-    return quote_results
-
-
-
-def process_results(quote_list):
-    '''
-    Function  that processes the quotes result and transform them to a list of Objects
-
-    Args:
-        quote_list: A list of dictionaries that contain quote details
-
-    Returns :
-        quote_results: A list of quote objects
-    '''
-    quote_results = []
-    for quote_item in quote_list:
-        id = quote_item.get('id')
-        author = quote_item.get('author')
-        quote = quote_item.get('quote') 
-
-        quote_object = Quote(id,author,quote)
-        quote_results.append(quote_object)
 
     return quote_results
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from flask import render_template
 from app import app
+from .request import get_quotes
 
 #Views
 @app.route("/")
 def index():
-
-    return render_template("index.html")
+    #Getting our quotes
+    quotes = get_quotes()
+    return render_template("index.html",quotes=quotes)
