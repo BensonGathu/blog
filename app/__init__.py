91f0dd65b5d96e7314b8507c42f8548ddefad4a6
Benson <bensongathu23@gmail.com>
2021-06-19 22:17:35 +0300

    email added

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 
 
@@ -11,6 +12,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -19,8 +21,7 @@ def create_app(config_name):
     #initializing the application
     app = Flask(__name__)
 
-    # configure UploadSet
-    configure_uploads(app,photos)
+    
 
     #setting up the configuration
     app.config.from_object(config_options[config_name])
@@ -30,6 +31,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
+
 
     #Registering the blueprint
     from .main import main as main_blueprint
@@ -38,8 +41,11 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
+    # configure UploadSet
+    configure_uploads(app,photos)
     #setting config
     from .request import configure_request
     configure_request(app)
+
     
     return app
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export SECRET_KEY='vcxz4321'
-
+export MAIL_USERNAME='unknownknowns23@gmail.com'
+export MAIL_PASSWORD='vcxz4321qwerty'
 python3 manage.py server
